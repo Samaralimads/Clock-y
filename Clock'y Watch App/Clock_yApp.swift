7182ec64b3d3140c6d3ce00d3e208127da2f0d03
“Samara <samara.limads@gmail.com>
2025-12-11 10:31:28 +0100

    first commit

---
+++ Clock'y Watch App/Clock_yApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clock_yApp.swift
+//  Clock'y Watch App
+//
+//  Created by Samara Lima da Silva on 11/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Clock_y_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clock'y Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clock'y Watch App
+//
+//  Created by Samara Lima da Silva on 11/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
